78d2947a12af15485f1c08f9900b8634b4844086
t0rn1k3 <tornikeburjanadze2@gmail.com>
2023-05-18 14:13:01 +0400

    add search component in main page and add some search logic on it

--- src/app/shared/components/tech-container/tech-container.component.ts
+++ src/app/shared/components/tech-container/tech-container.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { TechService } from '../../services/tech.service';
 import { TechItemsInterface } from '../../interfaces/tech-items.interface';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-tech-container',
@@ -13,11 +14,17 @@ export class TechContainerComponent implements OnInit {
   techItems : TechItemsInterface[] =[];
 
   constructor(
-    private techService: TechService
+    private techService: TechService,
+    private route : ActivatedRoute
   ){}
 
   ngOnInit(): void {
-    this.techItems = this.techService.getAllProd();
+    this.route.params.subscribe(params => {
+      if (params.searchTerm)
+        this.techItems = this.techService.getAllProd().filter(techItem => techItem.name.toLocaleLowerCase().includes(params.searchTerm.toLocaleLowerCase()))
+      else
+        this.techItems = this.techService.getAllProd();
+    })
   }
 
 }
